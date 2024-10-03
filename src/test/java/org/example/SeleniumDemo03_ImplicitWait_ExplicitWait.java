b0b10e343f015974d52ac0d3349a7effd2646299
ujha77 <ujha777@gmail.com>
2024-10-04 00:40:09 +0530

    ATB7x_4thOctober_SurefirePlugin

--- src/test/java/org/example/SeleniumDemo03_ImplicitWait_ExplicitWait.java
+++ src/test/java/org/example/SeleniumDemo03_ImplicitWait_ExplicitWait.java
@@ -15,7 +15,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 import java.time.Duration;
+
+
 
 public class SeleniumDemo03_ImplicitWait_ExplicitWait {
     WebDriver driver;
